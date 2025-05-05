7a3ad484b02aa677f1d2738cacfd5a261cd2f684
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-05-05 14:11:58 -0500

    Set up my constructors and getters and setters for my Reservation.java class

--- HotelOperations/src/main/java/com/pluralsight/Reservation.java
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
 public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
+
+    public double getPrice() {
+        double basePrice = roomType.equalsIgnoreCase("king") ? 139.00 : 124.00;
+        if (isWeekend) {
+            basePrice *= 1.10;
+        }
+        return basePrice;
+    }
+
+    public double getReservationTotal() {
+        return getPrice() * numberOfNights;
+    }
+
 }
+
+
